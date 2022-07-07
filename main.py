2aba2eac06706f11d10e50e45b6a2e693d085713
Michel <106674127+MichelZero@users.noreply.github.com>
2022-07-06 22:52:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def p(x): # função para calcular o valor de um polinômio de grau 4
 for x in [-1, 0, 2, 3.4]: # teste de valores para x para o polinômio de grau 4
     print(f'passando p({x}) = {p(x)}') # exibição do valor de p(x) para x = -1, 0, 2, 3.4 respectivamente
 
+print("_________________________________________________")
 ########################################################################################################################
 
 # 2 - exemplo de uso de passagem de varios argumentos
@@ -39,6 +40,28 @@ l2 = list(t2) # criação de uma lista a partir da tupla t2
 print(l2) # exibição da lista l2 com 5 elementos [1, 2, 3, 4, 5]
 print(type(l2)) # exibição do tipo da lista l2 <class 'list'>
 
+print("_________________________________________________")
+
 ########################################################################################################################
 
-# 3 -
+# 3 - Passando vários argumentos para um
+# método de instanciação de uma classe.
+# Quando precisamos passar uma indeterminada quantidades de paramentos para uma classe, método ou função, temos
+# basicamente a opções de uma coleção de forma posicional ou por chaves.
+# Para isso usamos o operador de descompactação ( *) para argumentos posicional e (**) para uso com chaves.
+# posicional: *args # argumentos (um interavel, lista, tupla, etc.)
+# chaves: **kwargs # argumentos comm chaves (um dicionário)
+#
+# exemplo de uso de passagem de varios argumentos em uma função:
+def func_args(*args):
+    print(f'tipo: {type(args)} conteúdo: {args}') # exibição do tipo e conteúdo da tupla *args
+    for arg in args:  # percorre os argumentos da tupla *args
+        print(f'tipo: {type(arg)} conteúdo: {arg}') # exibição do tipo e conteúdo dos argumentos da tupla *args
+
+# chamando a função func_args com varios argumentos
+func_args(1, 'A', {'valor': 10}) # passagem de 3 paramentos para a função func_args
+# um INT, um STRING e um DICIONÁRIO
+
+# 3.1 exemplo, criação de uma classe que recebe vários argumentos
+# class Equacao:
+
